44c2bbf0bd40e608915d3220106c5004b0d4084a
GoldingAustin <gol13014@byui.edu>
2017-01-14 21:47:28 -0700

    Updated homepage

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Location: https://whispering-shore-82912.herokuapp.com/hello.html"); /* Redirect browser */
+header("Location: https://whispering-shore-82912.herokuapp.com/index.html"); /* Redirect browser */
 exit();
 
 ?>
