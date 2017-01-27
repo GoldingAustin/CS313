25d8985737cdd598846a3ef03d809e6be5a4e5bc
GoldingAustin <gol13014@byui.edu>
2017-01-27 01:44:14 -0700

    testing password fix

--- web/easyChat/php/checkLogin.php
+++ web/easyChat/php/checkLogin.php
@@ -4,14 +4,13 @@ include("database.php");
 $myusername = pg_escape_string($conn, $_POST['name']);
 $mypassword = pg_escape_string($conn, $_POST['pass']);
 
-$sql = "SELECT * FROM easychat.user WHERE username = '$myusername'";
+$sql = "SELECT * FROM easychat.user WHERE username = '" . $myusername . "';";
 $result = pg_query($conn, $sql);
 $row = pg_fetch_array($result, PGSQL_ASSOC);
 $active = $row['active'];
 
 $count = pg_num_rows($result);
 $pass = pg_escape_string($conn, $row['password']);
-
 if ($count == 1) {
     if (password_verify($mypassword, $pass)) {
         if (isset($_SESSION)) {
