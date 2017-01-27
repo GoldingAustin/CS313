99eef1d329940e37af712ab375120a01caa2df26
GoldingAustin <gol13014@byui.edu>
2017-01-27 03:31:15 -0700

    acting different than checkUser although nearly identical

--- web/easyChat/php/checkLogin.php
+++ web/easyChat/php/checkLogin.php
@@ -3,19 +3,19 @@ session_start();
 include("database.php");
 $myusername = pg_escape_string($conn, $_POST['name']);
 $mypassword = pg_escape_string($conn, $_POST['pass1']);
-
+$mypassword = stripslashes($mypassword);
 $sql = "SELECT * FROM easychat.user WHERE username = '$myusername'";
 $result = pg_query($conn, $sql);
 $row = pg_fetch_array($result, PGSQL_ASSOC);
 $active = $row['active'];
 
 $count = pg_num_rows($result);
-$pass = stripslashes($row['password']);
+//$row["password"]);
 if ($count == 1) {
-    if (password_verify($_POST['pass1'], $pass)) {
+    if (password_verify($mypassword, $pass)) {
         if (isset($_SESSION)) {
             $_SESSION['login_user'] = $myusername;
-            $_SESSION['user_id'] = $row['user_id'];
+            $_SESSION['user_id'] = $row["user_id"];
         }
         echo "false";
         return false;
