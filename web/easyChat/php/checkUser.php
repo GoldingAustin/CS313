c9a5b5209274229a8bac9d6ecfd4b0e4876a4330
GoldingAustin <gol13014@byui.edu>
2017-01-27 00:12:09 -0700

    attempt to fix pgsql

--- web/easyChat/php/checkUser.php
+++ web/easyChat/php/checkUser.php
@@ -1,14 +1,13 @@
 <?php
 session_start();
 
-$conn = pg_connect("host=ec2-54-163-246-165.compute-1.amazonaws.com port=5432 dbname=dcfekv66t3orlp user=dxgkrpudzqxphn password=dbb68ad52247be47330ba3c1ae9d5e877ed1ee6a97f3fe33315580982eeef26d");
+include("database.php");
 //if(!$conn){  echo "Error : Unable to open database\n"; }
 //return $conn;
 $myusername = pg_escape_string($conn, $_POST['name']);
 $pass = password_hash($_POST['pass1'], PASSWORD_DEFAULT);
 //$pass = $_POST['pass1'];
 //$mypassword = pg_escape_string($conn, $pass);
-echo "<script>console.log('". $pass ."')</script>";
 $sql = "SELECT * FROM `user` WHERE username = '$myusername'";
 $result = pg_query($conn, $sql);
 $row = pg_fetch_array($result, PGSQL_ASSOC);
--- web/easyChat/php/database.php
+++ web/easyChat/php/database.php
@@ -6,7 +6,7 @@
 //$host = 'localhost:8889';
 //$port = 8889;
 
-$conn = pg_connect("host=localhost port=5432 dbname=easychat user=austingolding password=root");
+$conn = pg_connect("dbname=dcfekv66t3orlp host=ec2-54-163-246-165.compute-1.amazonaws.com port=5432 user=dxgkrpudzqxphn password=dbb68ad52247be47330ba3c1ae9d5e877ed1ee6a97f3fe33315580982eeef26d sslmode=require");
 
 // Create connection
 //$conn = new mysqli($host, $user, $password, $db);
