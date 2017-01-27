cdb8fd2cad763406b282b503e6cb89d9e418c85d
GoldingAustin <gol13014@byui.edu>
2017-01-27 01:59:00 -0700

    trying

--- web/easyChat/php/checkLogin.php
+++ web/easyChat/php/checkLogin.php
@@ -3,8 +3,8 @@ session_start();
 include("database.php");
 $myusername = pg_escape_string($conn, $_POST['name']);
 $mypassword = pg_escape_string($conn, $_POST['pass']);
-
-$sql = "SELECT * FROM easychat.user WHERE username = '" . $myusername . "';";
+$username = trim($myusername);
+$sql = "SELECT * FROM easychat.user WHERE username = '" . $username . "';";
 $result = pg_query($conn, $sql);
 $row = pg_fetch_array($result, PGSQL_ASSOC);
 $active = $row['active'];
@@ -12,14 +12,14 @@ $active = $row['active'];
 $count = pg_num_rows($result);
 $pass = pg_escape_string($conn, $row['password']);
 if ($count == 1) {
-    if (password_verify($mypassword, $pass)) {
+    if (password_verify($_POST['pass'], $pass)) {
         if (isset($_SESSION)) {
             $_SESSION['login_user'] = $myusername;
             $_SESSION['user_id'] = $row['user_id'];
         }
         echo "false";
     } else {
-        echo "Your Password is Invalid";
+        echo $pass;
     }
 } else {
     echo "Username or Password Incorrect";
