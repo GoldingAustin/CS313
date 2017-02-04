6f62c13403362b64f2e0d6665328a27f81728d20
GoldingAustin <gol13014@byui.edu>
2017-02-03 23:30:44 -0700

    update database connection

--- web/easyChat/php/database.php
+++ web/easyChat/php/database.php
@@ -5,13 +5,16 @@ $db = "dbname=austingolding";
 $host = "host=localhost";
 $port = "port=5432";
 
-$conn = pg_connect("dbname=dcfekv66t3orlp host=ec2-54-163-246-165.compute-1.amazonaws.com port=5432 user=dxgkrpudzqxphn password=dbb68ad52247be47330ba3c1ae9d5e877ed1ee6a97f3fe33315580982eeef26d sslmode=require");
 
-$conn = pg_connect("$host $port $db $user");
-if(!$conn){
-    //echo "Error : Unable to open database\n";
-} else {
-   // echo "Opened database successfully\n";
-}
+extract(parse_url($_ENV["DATABASE_URL"]));
+$conn = pg_connect("user=$user password=$pass host=$host dbname=" . substr($path, 1) . " sslmode=require");
+ //pg_connect("dbname=dcfekv66t3orlp host=ec2-54-163-246-165.compute-1.amazonaws.com port=5432 user=dxgkrpudzqxphn password=dbb68ad52247be47330ba3c1ae9d5e877ed1ee6a97f3fe33315580982eeef26d sslmode=require");
+
+//$conn = pg_connect("$host $port $db $user");
+//if(!$conn){
+//    //echo "Error : Unable to open database\n";
+//} else {
+//   // echo "Opened database successfully\n";
+//}
 
 ?>
