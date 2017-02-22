dce3aebffdedd776076e548f2ec2328d2ddd7877
GoldingAustin <austinjgolding>
2017-02-21 18:43:00 -0700

    add java servlet

---
+++ javaServlet/src/helloWorld/HelloServlet.java
@@ -0,0 +1,16 @@
+package helloWorld;
+
+import java.io.IOException;
+
+/**
+ * Created by austingolding on 2/21/17.
+ */
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
